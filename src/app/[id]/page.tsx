f4d11330a86b6edd9b1e16d77e26b0c0d09947d7
uzair-7886 <jerry.khan7886@gmail.com>
2024-06-06 16:39:50 +0500

    fix type errors

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -9,17 +9,17 @@ type Props = {
     searchParams: { [key: string]: string | string[] | undefined };
 };
 
-const Page = async ({ searchParams }: Props) => {
-    const idString = searchParams?.id;
+const Page = ({ searchParams }: Props) => {
+    // Extract id from searchParams and ensure it's a string
+    const idString = Array.isArray(searchParams?.id) ? searchParams.id[0] : searchParams?.id || '';
 
     return (
         <>
-        <Navbar/>
-        <div className='px-20 py-20 flex flex-col  items-start'>
-        <Link href="/" className='py-5 hover:underline font-bold'>Back</Link>
-
-        <ProductDetails id={idString}/>
-        </div>
+            <Navbar/>
+            <div className='px-20 py-20 flex flex-col items-start'>
+                <Link href="/" className='py-5 hover:underline font-bold'>Back</Link>
+                <ProductDetails id={idString}/>
+            </div>
         </>
     );
 };
